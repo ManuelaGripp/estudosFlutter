23b658f83b93cab832a0732f2ef38b234e85153e
ManuelaGripp <manugrippsilva@hotmail.com>
2022-11-29 16:16:40 -0300

    login view

--- devagram/lib/main.dart
+++ devagram/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:devagram/View/LoginView.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -31,34 +32,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _counter = '0';
-
   void _incrementCounter() {
-    setState(() {
-      _counter = dotenv.env['API_BASE_URL']!;
-    });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              'teste ' + _counter,
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      body: LoginView(),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
